84e3bf0d6f1e209214c40562fae4d6224661f5ff
GmailMoc <marijka.ona@gmail.com>
2020-12-20 17:48:39 +0200

    add homework 3

---
+++ homework3/WebApp_HR3_MVC/WebApp_HR3_MVC/Controllers/NewsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp_HR3_MVC.Models;
+
+namespace WebApp_HR3_MVC.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.News = NewsBase.News;
+
+            return View();
+        }
+    }
+}
---
+++ homework3/WebApp_HR3_MVC/WebApp_HR3_MVC/Models/News.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebApp_HR3_MVC.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string AuthorName { get; set; }
+        public bool IsFake { get; set; }
+    }
+}
