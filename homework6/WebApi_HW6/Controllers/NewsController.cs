d0d80834b07095c4ea12231651f88047f3a7a309
GmailMoc <marijka.ona@gmail.com>
2021-01-03 13:51:33 +0200

    edit appveyor file and add homework #6

---
+++ homework6/WebApi_HW6/Controllers/NewsController.cs
@@ -0,0 +1,114 @@
+﻿using BasicInfo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicInfo.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        private INewsRepository _newsRepository { get; }
+
+        public NewsController(INewsRepository newsRepository)
+        {
+            _newsRepository = newsRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Index([FromQuery] string filterId, [FromQuery] string filterAuthor, [FromQuery] string filterFake)
+        {
+            List<News> news = _newsRepository.GetNews();
+            try
+            {
+                if (!string.IsNullOrEmpty(filterId))
+                    news = news.Where(n => n.Id == int.Parse(filterId)).ToList();
+
+                if (!string.IsNullOrEmpty(filterAuthor))
+                    news = news.Where(n => n.AuthorName == filterAuthor).ToList();
+
+                if (!string.IsNullOrEmpty(filterFake))
+                    news = news.Where(n => n.IsFake == bool.Parse(filterFake)).ToList();
+
+                //return Ok(_newsRepository.GetNews());
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return Ok(news);
+        }
+
+        [HttpPost]
+        public IActionResult AddNews([FromBody] News news)
+        {
+            try
+            {
+                _newsRepository.AddNews(news);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateNews([FromBody] News news)
+        {
+            try
+            {
+                _newsRepository.DeleteNews(news.Id);
+                _newsRepository.AddNews(news);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return Ok();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult UpdateNews(int id, [FromBody] JsonPatchDocument<News> patchDoc)
+        {
+            if (patchDoc != null)
+            {
+                News news = _newsRepository.GetNews().First(x => x.Id == id);
+
+                patchDoc.ApplyTo(news, ModelState);
+
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                return new ObjectResult(news);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteNews([FromHeader] int newsId)
+        {
+            try
+            {
+                _newsRepository.DeleteNews(newsId);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ homework6/WebApi_HW6/Models/MockNewsRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicInfo.Models
+{
+    public class MockNewsRepository : INewsRepository 
+    {
+        private List<News> _news = new List<News>
+        {
+            new News { Id = 0, Title = "Humanity finally colonized the Mercury!!", Text = "", AuthorName = "Jeremy Clarkson", IsFake = true},
+            new News { Id = 1, Title = "Increase your lifespan by 10 years, every morning you need...", Text = "", AuthorName = "Svetlana Sokolova", IsFake = true},
+            new News { Id = 2, Title = "Scientists estimed the time of the vaccine invension: it is a summer of 2021", Text = "", AuthorName = "John Jones", IsFake = false},
+            new News { Id = 3, Title = "Ukraine reduces the cost of its obligations!", Text = "", AuthorName = "Cerol Denvers", IsFake = false},
+            new News { Id = 4, Title = "A species were discovered in Africa: it is blue legless cat", Text = "", AuthorName = "Jimmy Felon", IsFake = true}
+        };
+
+        public List<News> GetNews()
+        {
+            return _news;
+        }
+
+        public void AddNews(News news)
+        {
+            if (_news.FirstOrDefault(x => x.Id == news.Id) == null)
+                _news.Add(news);
+
+            //в else можно было бы выкидывать Exception, но я подумал, что это лишнее, так как мы просто считаем что элемент уже был добавлен ранее и не добавляем заново. 
+        }
+
+        public void DeleteNews(int id)
+        {
+            News news = _news.Single(x => x.Id == id);
+            _news.Remove(news);
+        }
+    }
+}
